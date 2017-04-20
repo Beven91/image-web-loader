f8d0781449628d44da13025b08a1f8035d73fa39
beven.zhou <beven.zhou@lianshang.com>
2017-04-20 13:22:41 +0800

    调整plugin.js 添加getRequest

--- src/plugin.js
+++ src/plugin.js
@@ -13,19 +13,19 @@ var fs = require('fs')
  */
 function RequireImageXAssetPlugin (imageAssets) {
   this.imageAssets = imageAssets
-  this.extensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp']
+  this.extensions = ['', '.jpg', '.jpeg', '.png', '.gif', '.bmp']
 }
 
 /**
  * 插件执行函数
  */
 RequireImageXAssetPlugin.prototype.apply = function (compiler) {
+  var thisContext = this
   compiler.plugin('compilation', function (compilation, params) {
     params.normalModuleFactory.plugin('before-resolve', function (value, next) {
-      var request = value.request
-      var element = request.replace(/^-?!+/, '').replace(/!!+/g, '!').split('!')
-      if (element.length == 2 && element[0] == 'image') {
-        value.request = this.find(element[1]) || request
+      var abspath = thisContext.getRequest(value.request, value.context)
+      if (abspath) {
+        value.request = 'image!' + abspath
       }
       next(null, value)
     })
@@ -33,18 +33,43 @@ RequireImageXAssetPlugin.prototype.apply = function (compiler) {
 }
 
 /**
+ * 获取需要处理的资源
+ * @param request 请求的资源
+ * @param context 引用模块的目录
+ */
+RequireImageXAssetPlugin.prototype.getRequest = function (request, context) {
+  var element = (request || '').replace(/^-?!+/, '').replace(/!!+/g, '!').split('!')
+  if (element.length == 2 && element[0] == 'image') {
+    return this.find(element[1], context)
+  }
+}
+
+/**
  * 根据imageAssets去查找对应的图片资源文件路径
  * @param name 请求的资源名称 
+ * @param context 引用模块的目录
  */
-RequireImageXAssetPlugin.prototype.find = function (name) {
-  var imageAssets = this.imageAssets
+RequireImageXAssetPlugin.prototype.find = function (name, context) {
+  var imageAssets = this.isPath(name) ? context : this.imageAssets
   var extensions = this.extensions
   var abspath = null
   for (var i = 0,k = extensions.length;i < k;i++) {
-    abspath = path.join(imageAssets, name, extensions[i])
+    abspath = path.join(imageAssets, name + extensions[i])
     if (fs.existsSync(abspath)) {
       return abspath
     }
+  }
+}
+
+/**
+ * 判断模块路径是否为路径而不是名称
+ * @param name 模块名称或者路径
+ */
+RequireImageXAssetPlugin.prototype.isPath = function (name) {
+  if (path.isAbsolute(name) || name.split('/').length > 1) {
+    return true
+  }else {
+    return false
   }
 }
 
